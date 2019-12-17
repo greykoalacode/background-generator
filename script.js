3edef049e619856f2352da3bf396471d6873c6cc
Surya Madhavan <32214378+greykoalacode@users.noreply.github.com>
2019-12-17 23:17:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,23 +2,21 @@ var css=document.querySelector("h3");
 var color1=document.querySelector(".color1");
 var color2=document.querySelector(".color2");
 var body=document.getElementById("gradient");
+// to set initial color and its linear gradient code
+function firstTime()
+{
+	color1.value="#ffffff";
+	color2.value="#0611ff"
+	background();
+}
+
+// to set background as gradient and change h3 value
 function background()
 {
 	body.style.background="linear-gradient(to right, "+color1.value+", "+color2.value+")";
     css.textContent = body.style.background+";";
 }
- background();
-
-
-var shuffle= document.querySelector("#shuffle");
-shuffle.addEventListener("click",generatr);
-
-function change()
-{
-color1.addEventListener("input",background);
-color2.addEventListener("input",background);
-}
-change();
+// to get random color
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -27,9 +25,21 @@ function getRandomColor() {
   }
   return color;
 }
+//to generate random color
 function generatr()
 {
 	color1.value=getRandomColor();
 	color2.value=getRandomColor();
 	background();
 }
+// order
+firstTime();
+
+var shuffle= document.querySelector("#shuffle");
+shuffle.addEventListener("click",generatr);
+
+color1.addEventListener("input",background);
+color2.addEventListener("input",background);
+
+
+
